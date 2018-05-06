c0511dfebbcebf83c14bbf8d36cc3037e5385783
DESKTOP-T7N0EKE\vinic <viniciuscortat@hotmail.com>
2018-05-06 17:36:18 -0300

    Cenario Player (cubo) que se movimenta Chao com cor diferente onde sera implementado a troca de cor do player Script do Player com movimento implementado Material de cor 255-0-0-0 para o chao com cor Script da camera sem nada

---
+++ CamaleaoNinja/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    float Velocidade = 2f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+        transform.Translate(Input.GetAxis("Horizontal") * Velocidade * Time.deltaTime, 0f, Input.GetAxis("Vertical") * Velocidade * Time.deltaTime);
+    }
+
+}
+
+    

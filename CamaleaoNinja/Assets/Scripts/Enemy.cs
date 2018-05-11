5de2c8e3523b89a3144b8e6e42b92e767e3d52e7
DESKTOP-T7N0EKE\vinic <viniciuscortat@hotmail.com>
2018-05-10 21:34:08 -0300

    Cenario voltou a rotacao(0,0,0) Script de inimigo modificado para gerar movimento e printar quando em contato com o player Script de visao criado para printar quando em contato com o player

--- CamaleaoNinja/Assets/Scripts/Enemy.cs
+++ CamaleaoNinja/Assets/Scripts/Enemy.cs
@@ -7,16 +7,39 @@ public class Enemy : MonoBehaviour {
     public float VisionDistance;
     public float TimeReaction;
     public float Attackvelocity;
+    public float PontaEsquerda = 14;
+    public float PontaDireita = -14.71f;
+    public int _Direction = 1;
+    public float velocidade = 5;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        transform.Rotate(0, 180, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (_Direction == 1)
+        {
+            transform.Translate(Vector3.right * velocidade * Time.deltaTime, Space.World);
+            _Direction = 1;
+        }
+        if (this.transform.position.x > PontaEsquerda)
+        {
+            _Direction = 2;
+            transform.Rotate(0, 180, 0);
+        }
+        if (_Direction == 2)
+        {
+            transform.Translate(Vector3.right * -velocidade * Time.deltaTime, Space.World);
+            _Direction = 2;
+        }
+        if (this.transform.position.x < PontaDireita)
+        {
+            _Direction = 1;
+            transform.Rotate(0, 180, 0);
+        }
+    }
 
     void setPath()
     {
@@ -26,5 +49,10 @@ public class Enemy : MonoBehaviour {
     void AttackPlayer()
     {
 
+    }
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+            print("Player Morreu =(");
     }
 }

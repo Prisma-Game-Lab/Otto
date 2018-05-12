d9d564ca8ea55e26b98174495ac4bc8aadc1dd8b
Leticia de Vicq <leticiadevicq@gmail.com>
2018-05-12 17:12:07 -0300

    minor changes

---
+++ CamaleaoNinja/Assets/cubinho.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubinho : MonoBehaviour {
+
+    public Animator playerAnim;
+
+	// Use this for initialization
+	void Start () {
+        playerAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space"))
+        {
+            playerAnim.SetTrigger("pula");
+
+        }
+	}
+}

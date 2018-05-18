3492e5c6113e2421ec8e0e99d5ead3e52f060756
Marcell666 <brucemarcellino@hotmail.com>
2018-05-17 23:11:32 -0300

    scripts camuflagem

---
+++ CamaleaoNinja/Assets/Scripts/Colored.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colored : MonoBehaviour
+{
+	[Header("Cor da Camuflagem")]
+	[Tooltip("Cor(material) para a qual o camaleao vai mudar ao estar em contato com este ojeto e tentar se camuflar.")]
+	public Material cor;
+	[Tooltip("Marque 'verdadeiro' se este objeto for capaz de dar ao camaleao uma nova cor")]
+	public bool ganhaCor = false;
+
+	// Use this for initialization
+    void Start()
+	{
+		if (cor == null)
+			cor = GetComponent<Renderer>().material;
+	}
+
+	private void OnCollisionEnter(Collision col)
+    {
+		Collider other = col.collider;
+        if (ganhaCor && other.tag == "Tongue")
+        {
+			other.GetComponent<ChangeColor>().AddColor(cor);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+	// Update is called once per frame
+	void Update()
+	{}
+}

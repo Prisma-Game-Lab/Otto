0f0c6cd644e5ae9960221d7fdcb7a7dd9c044efc
Michelle Santiago Beadle <michelle.santiago10@gmail.com>
2018-05-19 14:12:17 -0300

    Movimentação do player ajustada com a camera e camera seguindo o player, falta rotação

--- CamaleaoNinja/Assets/Scripts/Camera.cs
+++ CamaleaoNinja/Assets/Scripts/Camera.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Camera : MonoBehaviour {
 
     public GameObject Player;
-    public Vector3 distanciaPlayer = new Vector3(0, 15, 20);
+    //[Tooltip("Se for -1 ele usará a distância atual da camera")]
+    //public Vector3 distanciaPlayer = new Vector3(-1,-1,-1);
 
+    private Vector3 _offset;        
 
     // Use this for initialization
     void Start () {
-        this.transform.position = Player.transform.position + distanciaPlayer;
+
+        _offset = transform.position - Player.transform.position;
+
     }
 
     // Update is called once per frame
@@ -20,7 +24,7 @@ public class Camera : MonoBehaviour {
 
     private void followPlayer()
     {
-        this.transform.position = Player.transform.position + distanciaPlayer;
+        this.transform.position = Player.transform.position + _offset;
     }
 
 }
--- CamaleaoNinja/Assets/Scripts/Player.cs
+++ CamaleaoNinja/Assets/Scripts/Player.cs
@@ -7,16 +7,24 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     public float velocity = 2f;
+    public float velocityRotation = 0.15F;
     public float MaxStamina;
     public float TongueDistance;
     public int Lifes;
 
+    public Transform cam;
+
     private float _currentStamina;
     private Vector3 _lastCheckpoint;
+    private Vector3 _controlRight;
+    private Vector3 _controlForward;
+
 
 
     private void Start()
     {
+        _controlRight = Vector3.Cross(cam.up, cam.forward);
+        _controlForward = Vector3.Cross(cam.right, Vector3.up);
     }
 
     private void Update()
@@ -26,7 +34,23 @@ public class Player : MonoBehaviour
 
     public void move()
     {
-        transform.Translate(-1*Input.GetAxis("Horizontal") * velocity * Time.deltaTime * -1, 0f,-1*Input.GetAxis("Vertical") * velocity * Time.deltaTime * -1);
+        float moveHorizontal = Input.GetAxis("Horizontal") * velocity * Time.deltaTime * -1;
+        float moveVertical = Input.GetAxis("Vertical") * velocity * Time.deltaTime * -1;
+
+        Vector3 movement = (_controlRight * moveHorizontal) + (_controlForward * moveVertical);
+
+        Vector3 targetDirection = new Vector3(moveHorizontal, 0f, moveVertical);
+        targetDirection = cam.transform.TransformDirection(targetDirection);
+        targetDirection.y = 0.0f;
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        // Quaternion.LookRotation(cam.forward,cam.up)
+        //transform.rotation = Quaternion.LookRotation(movement);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+        //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, cam.localEulerAngles.y, transform.localEulerAngles.z);
+        //  transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+
+        transform.Translate(movement);
     }
 
     public void cloak()

ae8d30acde58b632b20be5f0924918e06f76f709
ropumar <rodrigopumar@gmail.com>
2018-05-29 21:41:46 -0300

    Revert "Revert "Merge branch 'master' of https://github.com/Rio-PUC-Games/CamaleaoNinja""
    This reverts commit 047563f89c60ea3b88d72de3f00c7c9270a90203.

---
+++ Assets/SoundManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagement : MonoBehaviour {
+
+    public Transform playertransform;
+    public GameObject pre;
+    public GameObject post;
+
+	// Update is called once per frame
+	void Update () {
+		if(playertransform.position.y<0)
+        {
+            pre.SetActive(false);
+            post.SetActive(true);
+        }
+	}
+}

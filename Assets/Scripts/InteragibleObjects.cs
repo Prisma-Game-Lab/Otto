88043d3cabb24bde5e718b30245d101ca9c66862
Matheus Homsi <mlhomsi@gmail.com>
2018-07-06 12:44:27 -0300

    Correcao depth of field

--- Assets/Scripts/InteragibleObjects.cs
+++ Assets/Scripts/InteragibleObjects.cs
@@ -8,12 +8,19 @@ public class InteragibleObjects : Objects {
     public float SpeedObject = 5f;
     public float SpeedPlayer = 5f;
 
+    [HideInInspector]
+    public bool isFollowingPlayer = false;
+
+    private Transform originalParent;
+
     // Use this for initialization
     void Start () {
 		if (Fixed == true)
         {
             gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         }
+
+        originalParent = this.transform.parent;
     }
 	
 	// Update is called once per frame
@@ -30,6 +37,14 @@ public class InteragibleObjects : Objects {
     public void pullObject(Transform player)
     {
         // Objeto fica preso na lingua do player
+        isFollowingPlayer = true;
         this.transform.parent = player.transform;
+    }
+
+    // solta o objeto da lingua
+    public void freeObjectFromtongue()
+    {
+        isFollowingPlayer = false;
+        this.transform.parent = originalParent;
     }
 }

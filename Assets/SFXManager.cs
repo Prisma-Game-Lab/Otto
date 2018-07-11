92613eb436193ee05aa3d69f363a1260e8d55f89
Matheus Homsi <mlhomsi@gmail.com>
2018-07-11 16:26:00 -0300

    Mais coisa do sistema de som

--- Assets/SFXManager.cs
+++ Assets/SFXManager.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SFXManager : MonoBehaviour {
-    
+
+    public GameObject Mar;
+    public GameObject Vento;
+    public GameObject Lingua;
+    public GameObject Passos;
+
+
 
 	// Use this for initialization
 	void Start () {

cff183ad7d01ac4372a89f63b5b27d4cdb5d683b
Marcell <brucemarcellino@hotmail.com>
2018-07-12 14:37:40 -0300

    nova hud

--- Assets/Scripts/Colored.cs
+++ Assets/Scripts/Colored.cs
@@ -12,9 +12,9 @@ public class Colored : MonoBehaviour
 
     public enum Corenum
     {
+		amarelo,
         verde,
-        azul,
-        vermelho
+        azul
     };
     [Tooltip("essencia cor que o player precisa ter para se camuflar nesse objeto")]
     public Corenum cores;
@@ -55,13 +55,13 @@ public class Colored : MonoBehaviour
 
         switch (cor)
         {
-            case Corenum.vermelho:
-                return new Color((float)193 / 255, (float)39 / 255, (float)64 / 255);
-            case Corenum.verde:
-                return new Color(0, (float)205 / 255, (float)7 / 255);
+            case Corenum.amarelo:
+				return new Color((float)255 / 255, (float)255 / 255, (float)0 / 255);
             case Corenum.azul:
                 return new Color((float)39 / 255, (float)64 / 255, (float)193 / 255);
+            case Corenum.verde:
+                return new Color(0, (float)205 / 255, (float)7 / 255);
         }
-        return Color.black;
+		return new Color((float)167 / 255, (float)254 / 255, (float)241 / 255);
     }
 }

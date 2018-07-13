4f48071a7bdeacc8e053c04d910d0d5b6d15af15
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-07-13 13:08:02 -0300

    Script

---
+++ Assets/Scripts/AtivaPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtivaPlayer : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject Inimigos;
+
+	// Use this for initialization
+	void Start () {
+		
+        player.SetActive(true);
+
+        foreach (Transform child in Inimigos.GetComponentInChildren<Transform>())
+        {
+            child.gameObject.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
